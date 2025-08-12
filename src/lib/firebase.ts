c77270f823421a949f03a6ac3d6e59d61e0a38c4
Joe O <lighningblue1@gmail.com>
2025-08-12 23:43:23 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,18 @@
+'use client';
+
+import { initializeApp, getApp, getApps } from 'firebase/app';
+
+const firebaseConfig = {
+  projectId: 'leadergrid3',
+  appId: '1:203876925697:web:ae7d331d075cdf31f81b00',
+  storageBucket: 'leadergrid3.firebasestorage.app',
+  apiKey: 'AIzaSyC-lpIVUm3ZEmysGOD_2D6J1ZkAB-sdbn4',
+  authDomain: 'leadergrid3.firebaseapp.com',
+  measurementId: '',
+  messagingSenderId: '203876925697',
+};
+
+// Initialize Firebase
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+
+export { app };
