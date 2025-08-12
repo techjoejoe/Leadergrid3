e0ca6b778e831b93276b084fc96e213d36ef61d7
Joe O <lighningblue1@gmail.com>
2025-08-12 23:48:07 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@
 import { initializeApp, getApp, getApps } from 'firebase/app';
 
 const firebaseConfig = {
-  projectId: 'leadergrid3',
-  appId: '1:203876925697:web:ae7d331d075cdf31f81b00',
-  storageBucket: 'leadergrid3.firebasestorage.app',
-  apiKey: 'AIzaSyBqS4DPA99x_B2bIs2ORkRz4sL5s0f4a-E',
-  authDomain: 'leadergrid3.firebaseapp.com',
-  measurementId: '',
-  messagingSenderId: '203876925697',
+  "projectId": "leadergrid3",
+  "appId": "1:203876925697:web:ae7d331d075cdf31f81b00",
+  "storageBucket": "leadergrid3.firebasestorage.app",
+  "apiKey": "AIzaSyC-lpIVUm3ZEmysGOD_2D6J1ZkAB-sdbn4",
+  "authDomain": "leadergrid3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "203876925697"
 };
 
 // Initialize Firebase
