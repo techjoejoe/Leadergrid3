4b8276e7eb2ac6838eb378883cb4ee7ffc9b3d1d
Joe O <lighningblue1@gmail.com>
2025-08-13 12:02:05 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,13 @@ import Link from 'next/link';
 export function Logo() {
   return (
     <Link href="/">
-      <Image src="/LeaderGridMascotLogo.png" alt="LeaderGrid Mascot" width={40} height={40} priority />
+      <Image 
+        src="/LeaderGridMascotLogo.png" 
+        alt="LeaderGrid Mascot" 
+        width={40} 
+        height={40} 
+        priority 
+      />
     </Link>
   );
 }
