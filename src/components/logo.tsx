3370c02043eeee53c0f4006ae772e80bf4d9f996
Joe O <lighningblue1@gmail.com>
2025-08-13 11:59:53 +0000

    LeaderGridMascotLogo.png is in the Public folder why is it not showing

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export function Logo() {
   return (
     <Link href="/" className="flex items-center font-headline text-2xl font-bold text-primary">
-      <Image src="/LeaderGridMascot.png" alt="LeaderGrid Mascot" width={40} height={40} priority />
+      <Image src="/LeaderGridMascotLogo.png" alt="LeaderGrid Mascot" width={40} height={40} priority />
     </Link>
   );
 }
