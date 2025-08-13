1483f85f775c09718a73a99cedb0536a646d9d0a
Joe O <lighningblue1@gmail.com>
2025-08-13 12:01:03 +0000

    Its not. Do not stop until you fix this

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export function Logo() {
   return (
-    <Link href="/" className="flex items-center font-headline text-2xl font-bold text-primary">
+    <Link href="/">
       <Image src="/LeaderGridMascotLogo.png" alt="LeaderGrid Mascot" width={40} height={40} priority />
     </Link>
   );
