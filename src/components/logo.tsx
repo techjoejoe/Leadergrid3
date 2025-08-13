f403e8bcf57c686ab888ba4eb99a837c0840b890
Joe O <lighningblue1@gmail.com>
2025-08-13 11:57:53 +0000

    Get the logo to show up correctly and reamove the word "Leadergrid" from

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,9 +3,8 @@ import Link from 'next/link';
 
 export function Logo() {
   return (
-    <Link href="/" className="flex items-center gap-2 font-headline text-2xl font-bold text-primary">
-      <Image src="/LeaderGridMascot.png" alt="LeaderGrid Mascot" width={30} height={30} priority />
-      <span>LeaderGrid</span>
+    <Link href="/" className="flex items-center font-headline text-2xl font-bold text-primary">
+      <Image src="/LeaderGridMascot.png" alt="LeaderGrid Mascot" width={40} height={40} priority />
     </Link>
   );
 }
