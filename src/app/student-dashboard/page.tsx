e83f645fc48aaa4b790636bd6bdf05bf80b36190
Joe O <lighningblue1@gmail.com>
2025-08-13 17:26:17 +0000

    Still not working

--- src/app/student-dashboard/page.tsx
+++ src/app/student-dashboard/page.tsx
@@ -192,7 +192,7 @@ export default function StudentDashboardPage() {
                                     </div>
                                 </DropdownMenuLabel>
                                 <DropdownMenuSeparator />
-                                <DropdownMenuItem onClick={() => setIsProfileEditorOpen(true)}>
+                                <DropdownMenuItem onSelect={(e) => { e.preventDefault(); setIsProfileEditorOpen(true); }}>
                                     <UserIcon className="mr-2 h-4 w-4" />
                                     <span>Edit Profile</span>
                                 </DropdownMenuItem>
