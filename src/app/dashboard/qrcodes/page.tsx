882298462138e2cfa9edf118b97a90d52c7fd30f
Joe O <lighningblue1@gmail.com>
2025-08-13 19:57:26 +0000

    Is it possible for us to have on this page a section where the trainer c

--- src/app/dashboard/qrcodes/page.tsx
+++ src/app/dashboard/qrcodes/page.tsx
@@ -1,9 +1,13 @@
 import { QrCodeGenerator } from "@/components/qr-code-generator";
+import { CheckInManager } from "@/components/check-in-manager";
+import { Separator } from "@/components/ui/separator";
 
 export default function QrCodesPage() {
     return (
-        <div>
+        <div className="space-y-8">
             <QrCodeGenerator />
+            <Separator />
+            <CheckInManager />
         </div>
     )
 }
