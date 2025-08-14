92f264c59622db3828922419cf9fd8dba2487b90
Joe O <lighningblue1@gmail.com>
2025-08-14 01:02:45 +0000

    Please remove the total point awarded active students badges earned in Q

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,10 +2,6 @@
 'use client';
 
 import {
-    Activity,
-    Award,
-    DollarSign,
-    Users,
     MoveRight,
     BookOpen,
     Loader2,
@@ -66,60 +62,6 @@ import { db } from "@/lib/firebase";
     return (
         <div className="flex flex-col gap-4">
             <h1 className="text-3xl font-headline font-bold">Dashboard</h1>
-            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-                <Card>
-                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium">
-                        Total Points Awarded
-                    </CardTitle>
-                    <DollarSign className="h-4 w-4 text-muted-foreground" />
-                    </CardHeader>
-                    <CardContent>
-                    <div className="text-2xl font-bold">45,231</div>
-                    <p className="text-xs text-muted-foreground">
-                        +20.1% from last month
-                    </p>
-                    </CardContent>
-                </Card>
-                <Card>
-                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium">
-                        Active Students
-                    </CardTitle>
-                    <Users className="h-4 w-4 text-muted-foreground" />
-                    </CardHeader>
-                    <CardContent>
-                    <div className="text-2xl font-bold">+2350</div>
-                    <p className="text-xs text-muted-foreground">
-                        +180.1% from last month
-                    </p>
-                    </CardContent>
-                </Card>
-                <Card>
-                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium">Badges Earned</CardTitle>
-                    <Award className="h-4 w-4 text-muted-foreground" />
-                    </CardHeader>
-                    <CardContent>
-                    <div className="text-2xl font-bold">+12,234</div>
-                    <p className="text-xs text-muted-foreground">
-                        +19% from last month
-                    </p>
-                    </CardContent>
-                </Card>
-                <Card>
-                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium">QR Scans Today</CardTitle>
-                    <Activity className="h-4 w-4 text-muted-foreground" />
-                    </CardHeader>
-                    <CardContent>
-                    <div className="text-2xl font-bold">+573</div>
-                    <p className="text-xs text-muted-foreground">
-                        +201 since last hour
-                    </p>
-                    </CardContent>
-                </Card>
-            </div>
              <Card>
                 <CardHeader>
                 <CardTitle className="font-headline">Active Classes</CardTitle>
