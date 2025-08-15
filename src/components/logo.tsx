542c421969a5d01ed0ef08bdac820effb7dae28e
Joe O <lighningblue1@gmail.com>
2025-08-15 19:40:48 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Mascot>
    
    with the

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,10 +1,10 @@
+import Image from 'next/image';
 import Link from 'next/link';
-import { Mascot } from './mascot';
 
 export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2" prefetch={false}>
-       <Mascot />
+       <Image src="/logo.png" alt="LeaderGrid Logo" width="150" height="32" />
     </Link>
   );
 }
