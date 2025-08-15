71a3519cfee2435e8d15ef5ebf0e90a4183ddd34
Joe O <lighningblue1@gmail.com>
2025-08-15 23:28:24 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,10 +1,9 @@
 import Image from 'next/image';
-import Link from 'next/link';
 
 export function Logo() {
   return (
-    <Link href="/" className="flex items-center gap-2" prefetch={false}>
+    <>
        <Image src="/logo.png" alt="LeaderGrid Logo" width="150" height="32" unoptimized />
-    </Link>
+    </>
   );
 }
