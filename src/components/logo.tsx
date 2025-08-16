40f4c7df365be391d99c5acdfb862851eba2a992
Joe O <lighningblue1@gmail.com>
2025-08-16 21:46:56 +0000

    Logo much larger

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export function Logo() {
   return (
     <>
-       <Image src="/logo.png" alt="LeaderGrid Logo" width="150" height="32" unoptimized />
+       <Image src="/logo.png" alt="LeaderGrid Logo" width="300" height="64" unoptimized />
     </>
   );
 }
