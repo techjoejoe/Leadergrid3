c2692eb156b1ff3e0c46d50adc0c788abb54e049
Joe O <lighningblue1@gmail.com>
2025-08-16 21:48:49 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,9 +1,14 @@
 import Image from 'next/image';
 
-export function Logo() {
+interface LogoProps {
+  width?: number;
+  height?: number;
+}
+
+export function Logo({ width = 150, height = 32 }: LogoProps) {
   return (
     <>
-       <Image src="/logo.png" alt="LeaderGrid Logo" width="150" height="32" unoptimized />
+       <Image src="/logo.png" alt="LeaderGrid Logo" width={width} height={height} unoptimized />
     </>
   );
 }
